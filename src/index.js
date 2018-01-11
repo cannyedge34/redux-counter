d04d6769d0d774e6385963abce40ef0dd851df63
1paciente <1paciente.com@gmail.com>
2018-01-11 18:39:49 +0100

    prod files

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import counterReducer from './store/reducers/counter';
 import resultReducer from './store/reducers/result';
 
@@ -23,4 +22,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-registerServiceWorker();
